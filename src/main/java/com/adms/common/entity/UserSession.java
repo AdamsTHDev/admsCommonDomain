71cbb98bbd2fb85a067a768fe5701eb3c7d45537
PataweeCAdmsTHDev <moth.pc@gmail.com>
2015-04-20 16:18:58 +0700

    Add UserSession for Authen service

---
+++ src/main/java/com/adms/common/entity/UserSession.java
@@ -0,0 +1,73 @@
+package com.adms.common.entity;
+
+import java.util.Date;
+
+import com.adms.common.domain.BaseDomain;
+
+public class UserSession extends BaseDomain {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3723094059069314710L;
+	
+	private String user;
+	private String userPassword;
+	private String userRole;
+	private Date userLoginDate;
+	private Boolean loginSuccess = false;
+	
+	public UserSession() {
+		
+	}
+	
+	public UserSession(String user) {
+		this.user = user;
+	}
+	
+	public UserSession(String user, String userPassword) {
+		this.user = user;
+		this.userPassword = userPassword;
+	}
+	
+	public String getUser() {
+		return user;
+	}
+	
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	public String getUserPassword() {
+		return userPassword;
+	}
+	
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+
+	public Date getUserLoginDate() {
+		return userLoginDate;
+	}
+
+	public void setUserLoginDate(Date userLoginDate) {
+		this.userLoginDate = userLoginDate;
+	}
+
+	public Boolean getLoginSuccess() {
+		return loginSuccess;
+	}
+
+	public void setLoginSuccess(Boolean loginSuccess) {
+		this.loginSuccess = loginSuccess;
+	}
+	
+}
